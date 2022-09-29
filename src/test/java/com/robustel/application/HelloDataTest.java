b5c059f62bd632a9a574c663b28446e8919f0049
yangxuehong <yangxh@robustel.cn>
2022-09-29 16:04:21 +0800

    1. 升级SpringBoot版本和JUnit单元测试版本 2. 更新单元测试插件

--- src/test/java/com/robustel/application/HelloDataTest.java
+++ src/test/java/com/robustel/application/HelloDataTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class HelloDataTest {
     @Test
     void Given_Empty_When_Hello_Then_DefaultGreeting() {
+        System.out.println(".........................");
         assertEquals("Hello, World!", HelloData.of(null).getGreeting());
     }
 
     @Test
     void Given_ANickname_When_Hello_Then_Expected() {
+        System.out.println(".........................");
         assertEquals("Hello, robustel!", HelloData.of("robustel").getGreeting());
     }
 }
